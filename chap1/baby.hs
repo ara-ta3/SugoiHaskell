80e859f29e896c73496ee16bbfcd82c8a6af0169
Arata Tanaka <tarata43@yahoo.co.jp>
2015-03-28 17:08:01 +0900

    リスト入門の手前まで

--- chap1/baby.hs
+++ chap1/baby.hs
@@ -1,2 +1,7 @@
 doubleMe x = x + x
-doubleUs x y = x*2 + y*2
+-- doubleUs x y = x*2 + y*2
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber x = if x > 100 then x else doubleMe x
+doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
+conanO'Brien = "It's a-me, Conan O'Brien!"
