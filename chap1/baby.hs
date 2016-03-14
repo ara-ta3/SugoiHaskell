bd1c898fd8f223277abfa2e557e49d4b73934e73
Arata Tanaka <tarata43@yahoo.co.jp>
2016-03-14 19:58:18 +0900

    Add error pattern

--- chap1/baby.hs
+++ chap1/baby.hs
@@ -5,3 +5,10 @@ doubleUs x y = doubleMe x + doubleMe y
 doubleSmallNumber x = if x > 100 then x else doubleMe x
 doubleSmallNumber' x = (if x > 100 then x else doubleMe x) + 1
 conanO'Brien = "It's a-me, Conan O'Brien!"
+
+-- Hoge = "hogehoge"
+-- ghci> :r
+-- [1 of 1] Compiling Main             ( baby.hs, interpreted )
+-- 
+-- baby.hs:9:1: Not in scope: data constructor ‘Hoge’
+-- Failed, modules loaded: none.
